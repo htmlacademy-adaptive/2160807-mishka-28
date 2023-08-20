b03dea46ea66d7fd43566ca415893c478c87eba8
Артем <lol43rus@mail.ru>
2023-08-20 13:21:32 +0300

    Защита. попытка-2

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
-let navMain = document.querySelector('.main-nav__wrapper');
-let navToggle = document.querySelector('.main-nav__toggle');
+let navMain = document.querySelector('.main-header__nav-wrapper');
+let navToggle = document.querySelector('.main-header__nav-toggle');
 
-navMain.classList.remove('main-nav__wrapper--nojs');
+navMain.classList.remove('main-header__nav-wrapper--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav__wrapper--closed')) {
-    navMain.classList.remove('main-nav__wrapper--closed');
-    navMain.classList.add('main-nav__wrapper--opened');
+  if (navMain.classList.contains('main-header__nav-wrapper--closed')) {
+    navMain.classList.remove('main-header__nav-wrapper--closed');
+    navMain.classList.add('main-header__nav-wrapper--opened');
   } else {
-    navMain.classList.add('main-nav__wrapper--closed');
-    navMain.classList.remove('main-nav__wrapper--opened');
+    navMain.classList.add('main-header__nav-wrapper--closed');
+    navMain.classList.remove('main-header__nav-wrapper--opened');
   }
   });
